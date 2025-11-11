31c0c571c87ed8550df20b45406699e50bd6bf2a
Phillip Getto <phil@perceptive.io>
2025-11-11 17:31:22 -0500

    Add Node dependency installation to CI workflow and update package installation

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/views/**/*.{html,erb,rb}",
+    "./app/components/**/*.{html,erb,rb}",
+    "./app/helpers/**/*.rb",
+  ],
+}
