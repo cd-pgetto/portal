64812c383756b916fe83a163b432d0f7ce9d2348
Phillip Getto <phil@perceptive.io>
2025-11-15 08:49:06 -0500

    Rename user_menu spec to nav_bar_spec and add tests for home button

--- app/views/layouts/nav_bar/home_button.rb
+++ app/views/layouts/nav_bar/home_button.rb
@@ -1,6 +1,6 @@
 class Views::Layouts::NavBar::HomeButton < Components::Base
   def view_template
-    a(href: root_path, class: "btn btn-ghost text-xl") do
+    a(href: home_path, class: "btn btn-ghost text-xl") do
       img(src: asset_path("perceptive-logo-white.png"), class: "theme-dark h-8")
       img(src: asset_path("perceptive-logo-dark.png"), class: "theme-light h-8")
     end
