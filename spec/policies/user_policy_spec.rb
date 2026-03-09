ff886ffc379b5801f8163b6fc9800297272c3f07
Phillip Getto <phil@perceptive.io>
2026-03-09 14:40:52 -0400

    Refactor UserPolicy specs to consolidate show permission tests. Improve robustness of create as a regular user

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe UserPolicy, type: :policy do
     context "as a regular user" do
       let(:user) { create(:user) }
 
-      it { is_expected.not_to permit(user, user) }
+      it { is_expected.not_to permit(user, User.new) }
     end
 
     context "as a system admin" do
@@ -21,31 +21,7 @@ RSpec.describe UserPolicy, type: :policy do
     end
   end
 
-  permissions :show? do
-    context "without any user" do
-      it { is_expected.not_to permit(nil, User.new) }
-    end
-
-    context "as a regular user" do
-      let(:user) { create(:user) }
-
-      context "own record" do
-        it { is_expected.to permit(user, user) }
-      end
-
-      context "another user's record" do
-        it { is_expected.not_to permit(user, create(:another_user)) }
-      end
-    end
-
-    context "as a system admin" do
-      let(:user) { create_system_admin }
-
-      it { is_expected.to permit(user, User.new) }
-    end
-  end
-
-  permissions :edit?, :update? do
+  permissions :show?, :edit?, :update? do
     context "without any user" do
       it { is_expected.not_to permit(nil, User.new) }
     end
